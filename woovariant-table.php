533d8b8a1e36b4cd497adde2aae2f76ceb51c5f8
thisisalamin <thisismdalamin@gmail.com>
2025-02-27 14:04:48 +0600

    Adjust column widths in variant table for improved layout consistency

--- woovariant-table.php
+++ woovariant-table.php
@@ -79,11 +79,11 @@ function wc_product_table_shortcode($atts) {
             <!-- Table Header -->
             <thead>
                 <tr class="!border-b !border-gray-200 bg-[#FAFAFA]">
-                    <th style="width:120px !important" class="p-4 text-center font-semibold !border !border-gray-200">IMAGES</th>
-                    <th class="p-4 text-left font-semibold !border !border-gray-200 w-[400px]">PRODUCT</th>
-                    <th style="width:220px !important" class="p-4 text-center font-semibold !border !border-gray-200">PRICE</th>
-                    <th class="p-4 text-center font-semibold !border !border-gray-200">QTY</th>
-                    <th style="width:220px !important" class="text-center font-semibold !border !border-gray-200">OPTIONS</th>
+                    <th style="width:100px !important" class="p-4 text-center font-semibold !border !border-gray-200">IMAGES</th>
+                    <th style="width:50% !important" class="p-4 text-left font-semibold !border !border-gray-200 w-[400px]">PRODUCT</th>
+                    <th style="width:30% !important" class="p-4 text-center font-semibold !border !border-gray-200">PRICE</th>
+                    <th style="width:100px !important" class="p-4 text-center font-semibold !border !border-gray-200">QTY</th>
+                    <th style="width:180px !important" class="text-center font-semibold !border !border-gray-200">OPTIONS</th>
                 </tr>
             </thead>
             <!-- Table Body -->
